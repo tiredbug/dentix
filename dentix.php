8199e68fb1f712c614edd8df0c8b40480e88c3de
basoro <drg.faisol@basoro.org>
2015-12-02 08:15:16 +0700

    Update dentix.php

--- dentix.php
+++ dentix.php
@@ -37,7 +37,7 @@ add_action( 'plugins_loaded', 'load_dentix_textdomain' );
 
 add_action('admin_print_scripts', 'dentix_scripts' );
 function dentix_scripts() {
-    if ( 'dentix' === get_current_screen()->id ) {
+	if ( 'dentix' === get_current_screen()->id ) {
 
 		wp_enqueue_style( 'jquery-ui-style', plugins_url( 'css/jquery-ui.min.css', __FILE__ ));
 		wp_enqueue_style( 'jquery-datepicker-css', plugins_url( 'css/jquery.datePicker.css', __FILE__ ));
@@ -56,221 +56,178 @@ function dentix_scripts() {
 
 add_action('edit_form_after_title', 'dentix_move_metabox');
 function dentix_move_metabox() {
-    global $post, $wp_meta_boxes;
-    do_meta_boxes(get_current_screen(), 'advanced', $post);
-    unset($wp_meta_boxes['dentix']['advanced']);
+	global $post, $wp_meta_boxes;
+	do_meta_boxes(get_current_screen(), 'advanced', $post);
+	unset($wp_meta_boxes['dentix']['advanced']);
 }
-
-add_filter( 'enter_title_here', 'dentix_default_title' );
-function dentix_default_title( $title ){
-     $screen = get_current_screen();
-     if  ( $screen->post_type == 'dentix' ) {
-          return 'Enter Registration Number';
-     }
-} 
 
 add_action( 'posts_where_request', 'dentix_fields_search' );
 function dentix_fields_search($where) {
-    if ( is_search() ) {
-    global $wpdb, $wp;
-    // Dump value of $where to check preg_replace pattern
-    // print_r( $where );
-        $where = preg_replace(
-            "/($wpdb->posts.post_title LIKE '%{$wp->query_vars['s']}%')/i",
-            "$0 OR ($wpdb->postmeta.meta_value LIKE '%{$wp->query_vars['s']}%')",
-            $where
-        );
-        add_filter( 'posts_distinct_request', 'dentix_search_distinct' );
-        add_filter( 'posts_join_request', 'dentix_search_join' );
-    }
-    return $where;
+	if ( is_search() ) {
+	global $wpdb, $wp;
+	// Dump value of $where to check preg_replace pattern
+	// print_r( $where );
+	$where = preg_replace(
+		"/($wpdb->posts.post_title LIKE '%{$wp->query_vars['s']}%')/i", 
+		"$0 OR ($wpdb->postmeta.meta_value LIKE '%{$wp->query_vars['s']}%')", 
+		$where 
+	); 
+	add_filter( 'posts_distinct_request', 'dentix_search_distinct' );
+	add_filter( 'posts_join_request', 'dentix_search_join' );
+	}
+	return $where;
 }
 
 function dentix_search_join( $join ) {
-    global $wpdb;
-    return $join .= " LEFT JOIN $wpdb->postmeta ON ($wpdb->posts.ID = $wpdb->postmeta.post_id) ";
+	global $wpdb;
+	return $join .= " LEFT JOIN $wpdb->postmeta ON ($wpdb->posts.ID = $wpdb->postmeta.post_id) ";
 }
 
 function dentix_search_distinct( $distinct ) {
-    $distinct = "DISTINCT";
-    return $distinct;
+	$distinct = "DISTINCT";
+	return $distinct;
 }
 
 //add_filter( 'manage_edit-dentix_columns', 'edit_dentix_columns' ) ;
 function edit_dentix_columns($columns) {
-    $new = array ();
-    foreach($columns as $key => $title) {
-        if($key =='date')
-            $new['address'] = 'Address';
-        $new [$key] = $title;
-    }
-    return $new;
+	$new = array ();
+	foreach($columns as $key => $title) {
+	if($key =='date')
+		$new['address'] = 'Address';
+		$new [$key] = $title;
+	}
+	return $new;
 }
 
 add_action( 'manage_dentix_posts_custom_column', 'manage_dentix_columns', 10, 2 );
 function manage_dentix_columns( $column, $post_id ) {
 
-    // Get meta if exists
-    $address = get_post_meta( $post_id, 'address', true );
-    $phone_number = get_post_meta( $post_id, 'phone_number', true );
+	// Get meta if exists
+	$address = get_post_meta( $post_id, 'address', true );
+	$phone_number = get_post_meta( $post_id, 'phone_number', true );
 
-    switch( $column ) {
+	switch( $column ) {
 
-        case 'address' :
-            if ( empty( $address ) ) {
-                echo __( 'Unknown' );
-            } else {
-                echo $address;
-            }
-            break;
+	case 'address' :
+		if ( empty( $address ) ) {
+			echo __( 'Unknown' );
+		} else {
+			echo $address;
+		}
+	break;
 
-        case 'phone_number' :
-            if ( empty( $phone_number ) ) {
-                echo __( 'Unknown' );
-            } else {
-                echo $phone_number;
-            }
-            break;
-    }
-
+	case 'phone_number' :
+		if ( empty( $phone_number ) ) {
+			echo __( 'Unknown' );
+		} else {
+			echo $phone_number;
+		}
+	break;
+	}
 }
 
 add_filter( 'manage_edit-dentix_sortable_columns', 'dentix_sortable_columns' );
 function dentix_sortable_columns( $columns ) {
-    $columns['address'] = 'address';
-    $columns['phone_number'] = 'phone_number';
-    return $columns;
+	$columns['address'] = 'address'; 
+	$columns['phone_number'] = 'phone_number'; 
+	return $columns;
 }
 
 add_filter('the_title', 'dentix_full_name',10, 2);
 function dentix_full_name($title, $id) {
-  if('dentix' == get_post_type($id)) {
-      return get_post_meta( $id, 'full_name', true );
-   }
-  else {
-      return $title;
-  }
+	if('dentix' == get_post_type($id)) {
+		return get_post_meta( $id, 'full_name', true );
+	} else {
+	return $title;
+	}
 }
 
 function set_dentix_columns($columns) {
-    return array(
-        'cb' => '<input type="checkbox" />',
-        'title' => __('Full Name'),
-        'address' => __('Address'),
-        'phone_number' => __('Phone Number'),
-        'date' => __('Date')
-    );
+	return array(
+		'cb' => '<input type="checkbox" />',
+		'title' => __('Full Name'),
+		'address' => __('Address'),
+		'phone_number' => __('Phone Number'),
+		'date' => __('Date')
+	);
 }
 add_filter('manage_dentix_posts_columns' , 'set_dentix_columns');
 
-function dentix_glance_items( $items = array() ) {
-    $post_types = array( 'dentix' );
-    foreach( $post_types as $type ) {
-        if( ! post_type_exists( $type ) ) continue;
-        $num_posts = wp_count_posts( $type );
-        if( $num_posts ) {
-            $published = intval( $num_posts->publish + $num_posts->draft + $num_posts->pending );
-            $post_type = get_post_type_object( $type );
-            $text = _n( '%s ' . $post_type->labels->singular_name, '%s ' . $post_type->labels->name, $published );
-            $text = sprintf( $text, number_format_i18n( $published ) );
-            if ( current_user_can( $post_type->cap->edit_posts ) ) {
-            $output = '<a href="edit.php?post_type=' . $post_type->name . '">' . $text . '</a>';
-                echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
-            } else {
-            $output = '<span>' . $text . '</span>';
-                echo '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>';
-            }
-        }
-    }
-    return $items;
-}
-
-add_filter( 'dashboard_glance_items', 'dentix_glance_items', 10, 1 );
-
-/**
- * WordPress function for redirecting users on login based on user role
- */
 function my_login_redirect( $url, $request, $user ){
-    if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
-        if( $user->has_cap( 'edit_dentixs' ) ) {
-            $url = admin_url('index.php');
-        } else {
-            $url = admin_url();
-        }
-    }
-    return $url;
+	if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
+		if( $user->has_cap( 'edit_dentixs' ) ) {
+		$url = admin_url('index.php');
+		} else {
+			$url = admin_url();
+		}
+	}
+	return $url;
 }
 
 add_filter('login_redirect', 'my_login_redirect', 10, 3 );
 
-/*------------------------------------------------------------------------------------
-	remove quick edit for custom post type videos just to check if less mem consumption
-------------------------------------------------------------------------------------*/
 add_filter( 'post_row_actions', 'remove_row_actions', 10, 2 );
-function remove_row_actions( $actions, $post )
-{
-  global $current_screen;
+function remove_row_actions( $actions, $post ) {
+	global $current_screen;
 	if( $current_screen->post_type != 'dentix' ) return $actions;
-	//unset( $actions['edit'] );
-	//unset( $actions['view'] );
-	unset( $actions['trash'] );
-	unset( $actions['inline hide-if-no-js'] );
-	//$actions['inline hide-if-no-js'] .= __( '' );
-
+		//unset( $actions['edit'] );
+		//unset( $actions['view'] );
+		unset( $actions['trash'] );
+		unset( $actions['inline hide-if-no-js'] );
+		//$actions['inline hide-if-no-js'] .= __( '' );
 	return $actions;
 }
 
-function admin_tool_bar($wp_admin_bar){
-    global $wp_admin_bar;
-    $post_type = 'dentix';
-    $count = wp_count_posts($post_type);
-    $args = array(
-        'id' => 'mbe_testimonials_pending',
-        'href' => admin_url('/edit.php?post_status=pending&post_type='.$post_type, 'http'),
-        'parent' => 'top-secondary'
-    );
-    if($count->pending == 1){
-        $title = ' Patient Quewe';
-    } else{
-        $title = ' Patients Quewe';
-    }
-    $args['meta']['title'] = $title;
-    if($count->pending == 0){
-        $display = '<span class="mbe-ab-text">'.$count->pending.' '.$title.'</span>';
-    } else{
-        $display = '<span class="mbe-update-bubble">'.$count->pending.'</span><span class="mbe-ab-text-active">'.$title.'</span>';
-    }
-    $args['title'] = $display;
-    $wp_admin_bar->add_node($args);
+function admin_tool_bar ($wp_admin_bar) {
+	global $wp_admin_bar;
+	$post_type = 'dentix';
+	$count = wp_count_posts ($post_type);
+	$args = array (
+		'id' => 'mbe_testimonials_pending',
+		'href' => admin_url('/edit.php?post_status=pending&post_type='.$post_type, 'http'), 
+		'parent' => 'top-secondary' 
+	); 
+	if ($count->pending == 1) {
+		$title = ' Patient Quewe';
+	} else {
+		$title = ' Patients Quewe';
+	}
+	$args['meta']['title'] = $title; 
+	if($count->pending == 0) {
+		$display = '<span class="mbe-ab-text">'.$count->pending.' '.$title.'</span>'; 
+	} else {
+        	$display = '<span class="mbe-update-bubble">'.$count->pending.'</span><span class="mbe-ab-text-active">'.$title.'</span>'; 
+	}
+	$args['title'] = $display;
+	$wp_admin_bar->add_node($args);
 }
 
 add_action('wp_before_admin_bar_render', 'admin_tool_bar', 999);
 
-
 add_filter( 'add_menu_classes', 'show_pending_number');
 function show_pending_number( $menu ) {
-    $type = "dentix";
-    $status = "pending";
-    $num_posts = wp_count_posts( $type, 'readable' );
-    $pending_count = 0;
-    if ( !empty($num_posts->$status) )
-        $pending_count = $num_posts->$status;
+	$type = "dentix";
+	$status = "pending";
+	$num_posts = wp_count_posts( $type, 'readable' );
+	$pending_count = 0;
+	if ( !empty($num_posts->$status) )
+	$pending_count = $num_posts->$status;
 
-    // build string to match in $menu array
-    if ($type == 'post') {
-        $menu_str = 'edit.php';
-    // support custom post types
-    } else {
-        $menu_str = 'edit.php?post_type=' . $type;
-    }
+	// build string to match in $menu array
+	if ($type == 'post') {
+		$menu_str = 'edit.php'; 
+	// support custom post types
+	} else {
+		$menu_str = 'edit.php?post_type=' . $type;
+	}
 
-    // loop through $menu items, find match, add indicator
-    foreach( $menu as $menu_key => $menu_data ) {
-        if( $menu_str != $menu_data[2] )
-            continue;
-        $menu[$menu_key][0] .= " <span class='update-plugins count-$pending_count'><span class='plugin-count'>" . number_format_i18n($pending_count) . '</span></span>';
-    }
-    return $menu;
+	// loop through $menu items, find match, add indicator
+	foreach( $menu as $menu_key => $menu_data ) {
+		if( $menu_str != $menu_data[2] )
+		continue;
+		$menu[$menu_key][0] .= " <span class='update-plugins count-$pending_count'><span class='plugin-count'>" . number_format_i18n($pending_count) . '</span></span>';
+	}
+	return $menu;
 }
 
 ?>
