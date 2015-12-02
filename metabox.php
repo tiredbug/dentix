6802e04486be1eaa81928419a45ea9dc289a0661
basoro <drg.faisol@basoro.org>
2015-12-02 08:34:39 +0700

    Update metabox.php

--- metabox.php
+++ metabox.php
@@ -3,19 +3,19 @@
 add_action( 'add_meta_boxes', 'dentix_metabox');
 
 function dentix_metabox( $post ) {
-    add_meta_box('dentix-metabox', 'Dentix Metabox', 'render_dentix_metabox', 'dentix', 'advanced', 'high');
+	add_meta_box('dentix-metabox', 'Dentix Metabox', 'render_dentix_metabox', 'dentix', 'advanced', 'high');
 } 
 
 function render_dentix_metabox( $post) {
-    global $wpdb, $_profile, $_adult_odontogram_upper, $_adult_odontogram_lower, $_child_odontogram_upper, $_child_odontogram_lower;
+	global $wpdb, $_profile, $_adult_odontogram_upper, $_adult_odontogram_lower, $_child_odontogram_upper, $_child_odontogram_lower;
 
-$today="P".date("Ym");
-$recentRegNumber = $wpdb->get_var("SELECT meta_value from {$wpdb->postmeta} where meta_key = 'registration_number' order by post_id desc limit 1");
-$lastNumber = substr($recentRegNumber, 8, 4);
-$nextNumber = $lastNumber + 1;
-$nextRegNumber = $today.sprintf('%04s', $nextNumber);
+	$today="P".date("Ym");
+	$recentRegNumber = $wpdb->get_var("SELECT meta_value from {$wpdb->postmeta} where meta_key = 'registration_number' order by post_id desc limit 1");
+	$lastNumber = substr($recentRegNumber, 8, 4);
+	$nextNumber = $lastNumber + 1;
+	$nextRegNumber = $today.sprintf('%04s', $nextNumber);
 
-    $dentix_galleries = get_post_meta($post->ID, 'dentix_gallery', true);
+	$dentix_galleries = get_post_meta($post->ID, 'dentix_gallery', true);
 
 	wp_nonce_field( 'dentix_metabox_nonce', 'dentix_metabox_nonce' );
 
@@ -29,31 +29,31 @@ $nextRegNumber = $today.sprintf('%04s', $nextNumber);
 	echo '<div class="inside">';
 
 	echo '<table class="form-table">';
-	    foreach ($_profile as $field) {
+		foreach ($_profile as $field) {
 			$meta = get_post_meta($post->ID, $field, true);
 			echo '<tr>';
 			echo '<th><label for="' . $field . '"><strong>' . __(ucwords(str_replace("_", " ", $field))) . '</strong></label></th>';
-            if ( $field == 'picture' ) {
-			echo '<td><img src="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" id="picture_preview" /><br/> <input type="hidden" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="regular-text"  /><input type="button" name="upload-picture" id="upload-picture" value="Upload Picture" class="button-secondary"  /></td>';
-            } elseif ( $field == 'registration_number' ) {
-                if($meta == "") {
-			echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="' . $nextRegNumber . '" class="medium-text" readonly /></td>';
-			    } else {
-			echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="medium-text" readonly /></td>';
-			    }
-            } elseif ( $field == 'sex' ) { 
-            echo '<td><input type="radio" name="' . $field . '" id="' . $field . '_male"  value="Male"  ' . checked( $meta, 'Male', false) . ' /> Male ';
-            echo '<input type="radio" name="' . $field . '" id="' . $field . '_female"  value="Female" ' . checked( $meta, 'Female', false) . ' /> Female</td>';
-            } elseif ( $field == 'address' ) { 
-            echo '<td><textarea rows="3" name="' . $field . '" id="' . $field . '" class="large-text">', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '</textarea></td>';
-            } elseif ( $field == 'marriage' ) { 
-            echo '<td><input type="radio" name="' . $field . '" id="' . $field . '_single"  value="Single"  ' . checked( $meta, 'Single', false) . ' /> Single ';
-            echo '<input type="radio" name="' . $field . '" id="' . $field . '_married"  value="Married" ' . checked( $meta, 'Married', false) . ' /> Married</td>';
-            } elseif ( $field == 'date_of_birth' ) {
-			echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="medium-text"  placeholder="dd/mm/yyyy"/></td>';
-            } else {
-			echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="regular-text" /></td>';
-            }
+        		if ( $field == 'picture' ) {
+				echo '<td><img src="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" id="picture_preview" /><br/> <input type="hidden" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="regular-text"  /><input type="button" name="upload-picture" id="upload-picture" value="Upload Picture" class="button-secondary"  /></td>';
+        		} elseif ( $field == 'registration_number' ) {
+                		if($meta == "") {
+					echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="' . $nextRegNumber . '" class="medium-text" readonly /></td>';
+			    	} else {
+					echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="medium-text" readonly /></td>';
+			    	}
+            		} elseif ( $field == 'sex' ) { 
+            			echo '<td><input type="radio" name="' . $field . '" id="' . $field . '_male"  value="Male"  ' . checked( $meta, 'Male', false) . ' /> Male ';
+        			echo '<input type="radio" name="' . $field . '" id="' . $field . '_female"  value="Female" ' . checked( $meta, 'Female', false) . ' /> Female</td>';
+            		} elseif ( $field == 'address' ) { 
+            			echo '<td><textarea rows="3" name="' . $field . '" id="' . $field . '" class="large-text">', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '</textarea></td>';
+            		} elseif ( $field == 'marriage' ) { 
+            			echo '<td><input type="radio" name="' . $field . '" id="' . $field . '_single"  value="Single"  ' . checked( $meta, 'Single', false) . ' /> Single ';
+            			echo '<input type="radio" name="' . $field . '" id="' . $field . '_married"  value="Married" ' . checked( $meta, 'Married', false) . ' /> Married</td>';
+            		} elseif ( $field == 'date_of_birth' ) {
+				echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="medium-text"  placeholder="dd/mm/yyyy"/></td>';
+            		} else {
+				echo '<td><input type="text" name="' . $field . '" id="' . $field . '" value="', $meta ? $meta : stripslashes(htmlspecialchars(( ""), ENT_QUOTES)), '" class="regular-text" /></td>';
+            		}
 			echo '<tr>';
 		}
 	echo '</table>';
@@ -62,52 +62,52 @@ $nextRegNumber = $today.sprintf('%04s', $nextNumber);
 	echo '<div class="inside hidden">';
   	echo '<div class="table-odontogram">';
 	echo '<table style="margin: 20px auto; width: 450px; text-align: center;">';
-			echo '<tr>';
+		echo '<tr>';
 	        foreach ($_adult_odontogram_upper as $aou_num) {
 			echo '<td><strong>' . str_replace("gg_", "", $aou_num) . '</strong></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
-	        foreach ($_adult_odontogram_upper as $aou) {
+	   	}
+		echo '</tr>';
+		echo '<tr>';
+	       	foreach ($_adult_odontogram_upper as $aou) {
 			$meta = get_post_meta($post->ID, $aou, true);
 			echo '<td><input type="text" name="' . $aou . '" id="' . $aou . '" value="' . $meta . '" class="odont_input color" /></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
-	        foreach ($_adult_odontogram_lower as $aol) {
+	    	}
+		echo '</tr>';
+		echo '<tr>';
+		foreach ($_adult_odontogram_lower as $aol) {
 			$meta = get_post_meta($post->ID, $aol, true);
 			echo '<td><input type="text" name="' . $aol . '" id="' . $aol . '" value="' . $meta . '" class="odont_input color" /></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
-	        foreach ($_adult_odontogram_lower as $aol_num) {
+	    	}
+		echo '</tr>';
+		echo '<tr>';
+		foreach ($_adult_odontogram_lower as $aol_num) {
 			echo '<td><strong>' . str_replace("gg_", "", $aol_num) . '</strong></td>';
-		    }
-			echo '</tr>';
+	    	}
+		echo '</tr>';
 	echo '</table>';
 	echo '<table style="margin: 20px auto; width: 300px; text-align: center;">';
-			echo '<tr>';
+		echo '<tr>';
 	        foreach ($_child_odontogram_upper as $cou_num) {
 			echo '<td><strong>' . str_replace("gg_", "", $cou_num) . '</strong></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
+		}
+		echo '</tr>';
+		echo '<tr>';
 	        foreach ($_child_odontogram_upper as $cou) {
 			$meta = get_post_meta($post->ID, $cou, true);
 			echo '<td><input type="text" name="' . $cou . '" id="' . $cou . '" value="' . $meta . '" class="odont_input color" /></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
+		}
+		echo '</tr>';
+		echo '<tr>';
 	        foreach ($_child_odontogram_lower as $col) {
 			$meta = get_post_meta($post->ID, $col, true);
 			echo '<td><input type="text" name="' . $col . '" id="' . $col . '" value="' . $meta . '" class="odont_input color" /></td>';
-		    }
-			echo '</tr>';
-			echo '<tr>';
+		}
+		echo '</tr>';
+		echo '<tr>';
 	        foreach ($_child_odontogram_lower as $col_num) {
 			echo '<td><strong>' . str_replace("gg_", "", $col_num) . '</strong></td>';
-		    }
-			echo '</tr>';
+		}
+		echo '</tr>';
 	echo '</table>';
 ?>
 	<table style="margin: 30px auto 0 auto; width: 450px;">
@@ -138,20 +138,22 @@ $nextRegNumber = $today.sprintf('%04s', $nextNumber);
 
 	echo '<div class="inside hidden">';
 ?>
-    <table class="form-table">
-      <tr><td>
-        <a class="gallery-add button" href="#" data-uploader-title="Add image(s) to gallery" data-uploader-button-text="Add image(s)">Add image(s)</a>
-        <ul id="gallery-metabox-list">
-        <?php if ($dentix_galleries) : foreach ($dentix_galleries as $key => $value) : $image = wp_get_attachment_image_src($value); ?>
-          <li>
-            <input type="hidden" name="dentix_gallery[<?php echo $key; ?>]" value="<?php echo $value; ?>">
-            <img class="image-preview" src="<?php echo $image[0]; ?>">
-            <small><a class="remove-image" href="#">Remove image</a></small>
-          </li>
-        <?php endforeach; endif; ?>
-        </ul>
-      </td></tr>
-    </table>
+	<table class="form-table">
+      		<tr>
+      			<td>
+        		<a class="gallery-add button" href="#" data-uploader-title="Add image(s) to gallery" data-uploader-button-text="Add image(s)">Add image(s)</a>
+        		<ul id="gallery-metabox-list">
+        		<?php if ($dentix_galleries) : foreach ($dentix_galleries as $key => $value) : $image = wp_get_attachment_image_src($value); ?>
+          			<li>
+            				<input type="hidden" name="dentix_gallery[<?php echo $key; ?>]" value="<?php echo $value; ?>">
+            				<img class="image-preview" src="<?php echo $image[0]; ?>">
+            				<small><a class="remove-image" href="#">Remove image</a></small>
+          			</li>
+        		<?php endforeach; endif; ?>
+        		</ul>
+      			</td>
+      		</tr>
+    	</table>
 <?php
 	echo '</div>';
 	echo '</div>';
@@ -170,10 +172,10 @@ function save_dentix_metabox($post_id) {
             return;
         }
             
-    if(isset($_POST['post_type']) && $_POST['post_type'] == 'dentix' && current_user_can('edit_post', $post_id)) {
-    	foreach($_profile as $field) {
-    		update_post_meta($post_id, $field, $_POST[$field]);
-        }
+    	if(isset($_POST['post_type']) && $_POST['post_type'] == 'dentix' && current_user_can('edit_post', $post_id)) {
+    		foreach($_profile as $field) {
+    			update_post_meta($post_id, $field, $_POST[$field]);
+        	}
 
 		foreach ($_adult_odontogram_upper as $aou) {
 			$old = get_post_meta($post_id, $aou, true);
@@ -223,8 +225,7 @@ function save_dentix_metabox($post_id) {
   
 	} else {
 		return;
-    } 
+	} 
 } 
-
 
 ?>
