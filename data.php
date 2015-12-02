fd8dfe9a129952df9ee99b34aa18798731fd5e38
basoro <drg.faisol@basoro.org>
2015-12-02 08:25:16 +0700

    Create data.php

---
+++ data.php
@@ -0,0 +1,79 @@
+<?php
+
+$_profile = array (
+    'picture',
+    'registration_number',
+    'full_name',
+    'sex',
+    'date_of_birth',
+    'address',
+    'phone_number',
+    'occupation',
+    'marriage'
+);
+
+$_adult_odontogram_upper = array (
+    'gg_18',
+    'gg_17',
+    'gg_16',
+    'gg_15',
+    'gg_14',
+    'gg_13',
+    'gg_12',
+    'gg_11',
+    'gg_21',
+    'gg_22',
+    'gg_23',
+    'gg_24',
+    'gg_25',
+    'gg_26',
+    'gg_27',
+    'gg_28'
+);
+
+$_adult_odontogram_lower = array (
+    'gg_48',
+    'gg_47',
+    'gg_46',
+    'gg_45',
+    'gg_44',
+    'gg_43',
+    'gg_42',
+    'gg_41',
+    'gg_31',
+    'gg_32',
+    'gg_33',
+    'gg_34',
+    'gg_35',
+    'gg_36',
+    'gg_37',
+    'gg_38'
+);
+
+$_child_odontogram_upper = array (
+    'gg_55',
+    'gg_54',
+    'gg_53',
+    'gg_52',
+    'gg_51',
+    'gg_61',
+    'gg_62',
+    'gg_63',
+    'gg_64',
+    'gg_65'
+);
+
+$_child_odontogram_lower = array (
+    'gg_75',
+    'gg_74',
+    'gg_73',
+    'gg_72',
+    'gg_71',
+    'gg_81',
+    'gg_82',
+    'gg_83',
+    'gg_84',
+    'gg_85'
+);
+
+?>
